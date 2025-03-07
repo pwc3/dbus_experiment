407c63b6017ac37936115eb38d03dd8b9d5d7fdb
Paul Calnan <pwc3@users.noreply.github.com>
2025-03-07 10:09:33 -0500

    Added client

---
+++ src/dbus_experiment/client.py
@@ -0,0 +1,27 @@
+import dbus
+import dbus.service
+import time
+
+class Time(dbus.service.Object):
+    def __init__(self):
+        self.bus = dbus.SessionBus()
+        name = dbus.service.BusName("com.paulcalnan.Time", bus=self.bus)
+        super().__init__(name, "/Time")
+
+    @dbus.service.method("com.paulcalnan.Time", out_signature="s")
+    def current_time(self):
+        formatter = "%d-%m-%Y %H:%M:%S"
+        return time.strftime(formatter)
+
+if __name__ == "__main__":
+    import dbus.mainloop.glib
+    from gi.repository import GLib
+
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+    loop = GLib.MainLoop()
+    object = Time()
+
+    print("Started")
+    loop.run()
+
